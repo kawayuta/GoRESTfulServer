b0e7a77be85509ec55bab0ea336c27b4abf66aec
kawayuta <k216q013@stu.kyoto-seika.ac.jp>
2017-11-13 13:59:32 +0900

    add logger

--- handlers.go
+++ handlers.go
@@ -18,6 +18,8 @@ func TodoIndex(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 		Todo{Name: "歯磨きする"},
 	}
 
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
 	if err := json.NewEncoder(w).Encode(todos); err != nil {
 		panic(err)
 	}
--- main.go
+++ main.go
@@ -3,15 +3,15 @@ package main
 import (
 	"log"
 	"net/http"
-	
+
 	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
 	router := httprouter.New()
-	router.GET("/", Index)
-	router.GET("/todos", TodoIndex)
-	router.GET("/todos/:todoId", TodoShow)
+	router.GET("/", Logging(Index, "index"))
+	router.GET("/todos", Logging(TodoIndex, "todo-index"))
+	router.GET("/todos/:todoId", Logging(TodoShow, "todo-show"))
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
